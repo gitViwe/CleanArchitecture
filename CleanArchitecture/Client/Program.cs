0bd688b194b061e8d71c118ec54b2fdc4f4f5a35
Viwe Nkepu <viwe.nkepu@hotmail.com>
2021-11-28 11:32:01 +0200

    Initial Blazor Progressive web application setup

---
+++ CleanArchitecture/Client/Program.cs
@@ -0,0 +1,15 @@
+using Client;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+// add MudBlazor service https://mudblazor.com/getting-started/installation
+builder.Services.AddMudServices();
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
