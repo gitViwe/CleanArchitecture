a30fec8104bf0ff0f012a04e00dfdd5f370ef8e0
Viwe Nkepu <viwe.nkepu@hotmail.com>
2021-12-20 03:36:00 +0200

    Updated nuget packages

---
+++ CleanArchitecture/Client/Extensions/WebAssemblyHostBuilderExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace Client.Extensions
+{
+    /// <summary>
+    /// Implementation of the services registered in the <see cref="Program"/> class
+    /// </summary>
+    public static class WebAssemblyHostBuilderExtensions
+    {
+
+    }
+}
